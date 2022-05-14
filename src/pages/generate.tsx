bd7184fb474a991e74258e930d8bdf7dbb9e9ba3
Florent <florent@sqooba.io>
2022-05-14 21:51:41 +0200

    add v0 QR code generator

--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -1,8 +1,26 @@
-import React from "react"
-import { useSearchParams } from "react-router-dom"
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import QRCode from "react-qr-code";
+import ReactToPrint from "react-to-print";
 
 export const Generate: React.FC<{}> = () => {
-    const [params] = useSearchParams()
-    const clean = params.get("info") || "no info"
-    return <p>Hello, params: {clean}</p>
-}
+  const [params] = useSearchParams();
+  const clean = params.get("info");
+  const component = React.useRef<any>(null);
+
+  if (clean === null) {
+    return <p>No info, no QR Code.</p>;
+  }
+
+  return (
+    <div>
+      <ReactToPrint
+        trigger={() => <button>Print QRCode</button>}
+        content={() => component.current}
+      />
+      <div ref={component} style={{ width: "1cm" }}>
+        <QRCode size={64} value={clean} />
+      </div>
+    </div>
+  );
+};
